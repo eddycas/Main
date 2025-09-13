c8becf128421a7179870a982bf300fe940660529
eddycas <85770448+eddycas@users.noreply.github.com>
2025-09-13 17:26:28 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-    import 'dart:async';
+import 'dart:async';
 
 import 'dart:math';
 
@@ -98,7 +98,7 @@ class _QuickCalcAppState extends State<QuickCalcApp> {
 
       themeMode: _themeMode,
 
-      home: CalculatorHome(toggleTheme: _toggleTheme),
+      home: CalculatorHome(toggleTheme: _toggleTheme, themeMode: _themeMode),
 
     );
 
@@ -318,7 +318,9 @@ class AdsManager {
 
           interstitialAd = ad;
 
-          interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+          interstitialAd!.fullScreenContentCallback =
+
+              FullScreenContentCallback(
 
             onAdDismissedFullScreenContent: (ad) {
 
@@ -490,7 +492,9 @@ class CalculatorKeypad extends StatelessWidget {
 
   final void Function(String) onPressed;
 
-  const CalculatorKeypad({super.key, required this.onPressed});
+  final ThemeMode themeMode;
+
+  const CalculatorKeypad({super.key, required this.onPressed, required this.themeMode});
 
 
 
@@ -511,6 +515,30 @@ class CalculatorKeypad extends StatelessWidget {
       ['C', 'DEL', 'M+', 'M-', 'MR', 'MC']
 
     ];
+
+
+
+    Color getButtonColor(String btn) {
+
+      if (themeMode == ThemeMode.light) return Colors.white;
+
+      return Colors.black;
+
+    }
+
+
+
+    Color getTextColor(String btn) {
+
+      if (btn == 'DEL') return Colors.red;
+
+      if (['+', '-', '×', '÷'].contains(btn)) return Colors.blue;
+
+      if (themeMode == ThemeMode.light) return Colors.black;
+
+      return Colors.white; // invert for dark mode
+
+    }
 
 
 
@@ -540,13 +568,21 @@ class CalculatorKeypad extends StatelessWidget {
 
                     style: ElevatedButton.styleFrom(
 
-                      backgroundColor: Colors.amberAccent,
+                      shape: const CircleBorder(),
 
-                      foregroundColor: Colors.black,
+                      backgroundColor: getButtonColor(btn),
+
+                      foregroundColor: getTextColor(btn),
+
+                      shadowColor: Colors.black54,
+
+                      elevation: 5,
+
+                      padding: const EdgeInsets.all(20),
 
                     ),
 
-                    child: Text(btn, style: const TextStyle(fontSize: 24)),
+                    child: Text(btn, style: TextStyle(fontSize: 24, color: getTextColor(btn))),
 
                   ),
 
@@ -580,7 +616,9 @@ class CalculatorHome extends StatefulWidget {
 
   final VoidCallback toggleTheme;
 
-  const CalculatorHome({super.key, required this.toggleTheme});
+  final ThemeMode themeMode;
+
+  const CalculatorHome({super.key, required this.toggleTheme, required this.themeMode});
 
   @override
 
@@ -648,17 +686,11 @@ class CalculatorHomeState extends State<CalculatorHome> {
 
     premiumManager.loadPremium();
 
-
-
     _loadHistory();
-
-
 
     adsManager.loadTopBanner(onLoaded: () => setState(() => isTopBannerLoaded = true));
 
     adsManager.loadBottomBanner(onLoaded: () => setState(() => isBottomBannerLoaded = true));
-
-
 
     adsManager.loadRewardedAd(onLoaded: (ad) {
 
@@ -667,8 +699,6 @@ class CalculatorHomeState extends State<CalculatorHome> {
       isRewardedReady = true;
 
     });
-
-
 
     adsManager.loadInterstitial();
 
@@ -830,7 +860,15 @@ class CalculatorHomeState extends State<CalculatorHome> {
 
   Widget build(BuildContext context) {
 
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    final screenHeight = MediaQuery.of(context).size.height;
+
+
+
     return Scaffold(
+
+      backgroundColor: widget.themeMode == ThemeMode.light ? Colors.white : Colors.black,
 
       appBar: AppBar(
 
@@ -878,7 +916,13 @@ class CalculatorHomeState extends State<CalculatorHome> {
 
                     result,
 
-                    style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                    style: TextStyle(
+
+                        fontSize: 48,
+
+                        fontWeight: FontWeight.bold,
+
+                        color: widget.themeMode == ThemeMode.light ? Colors.black : Colors.white),
 
                   ),
 
@@ -886,7 +930,7 @@ class CalculatorHomeState extends State<CalculatorHome> {
 
               ),
 
-              Expanded(child: CalculatorKeypad(onPressed: handleCalculation)),
+              Expanded(child: CalculatorKeypad(onPressed: handleCalculation, themeMode: widget.themeMode)),
 
               if (isBottomBannerLoaded)
 
@@ -896,19 +940,21 @@ class CalculatorHomeState extends State<CalculatorHome> {
 
           ),
 
+
+
           // ================= History Panel (Left) =================
 
           AnimatedPositioned(
 
             duration: const Duration(milliseconds: 300),
 
-            left: leftPanelOpen ? 0 : -MediaQuery.of(context).size.width * 0.7,
+            left: leftPanelOpen ? 0 : -screenWidth * 0.7,
 
             top: 0,
 
             bottom: 0,
 
-            width: MediaQuery.of(context).size.width * 0.7,
+            width: screenWidth * 0.7,
 
             child: Container(
 
@@ -948,9 +994,7 @@ class CalculatorHomeState extends State<CalculatorHome> {
 
                       itemCount: history.length,
 
-                      itemBuilder: (context, index) =>
-
-                          ListTile(title: Text(history[index])),
+                      itemBuilder: (context, index) => ListTile(title: Text(history[index])),
 
                     ),
 
@@ -964,149 +1008,199 @@ class CalculatorHomeState extends State<CalculatorHome> {
 
           ),
 
+
+
           // ================= Premium Panel (Right) =================
 
           AnimatedPositioned(
 
             duration: const Duration(milliseconds: 300),
 
-            right: rightPanelOpen ? 0 : -MediaQuery.of(context).size.width * 0.7,
+            right: rightPanelOpen ? (screenWidth * 0.25) : -screenWidth * 0.5,
 
-            top: 0,
+            top: screenHeight * 0.25,
 
-            bottom: 0,
+            bottom: screenHeight * 0.25,
 
-            width: MediaQuery.of(context).size.width * 0.7,
+            width: screenWidth * 0.5,
 
-            child: Container(
+            child: ClipRRect(
 
-              color: Theme.of(context).colorScheme.surface,
+              borderRadius: BorderRadius.circular(20),
 
-              padding: const EdgeInsets.all(16),
+              child: Container(
 
-              child: Column(
+                color: Theme.of(context).colorScheme.surface,
 
-                children: [
+                padding: const EdgeInsets.all(16),
 
-                  Row(
+                child: Column(
 
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
 
-                    children: [
+                    Row(
 
-                      const Text("Premium",
-
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-
-                      IconButton(
-
-                          icon: const Icon(Icons.close),
-
-                          onPressed: () => setState(() => rightPanelOpen = false)),
-
-                    ],
-
-                  ),
-
-                  const Divider(),
-
-                  Expanded(
-
-                    child: Column(
-
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
                       children: [
 
-                        if (!premiumManager.isPremium)
+                        const Text("Premium",
 
-                          const Padding(
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
 
-                            padding: EdgeInsets.all(8.0),
+                        IconButton(
 
-                            child: Text("Not a Premium User. Unlock features by watching ads.",
+                            icon: const Icon(Icons.close),
 
-                                textAlign: TextAlign.center,
-
-                                style: TextStyle(fontSize: 16)),
-
-                          ),
-
-                        if (!premiumManager.isPremium)
-
-                          ElevatedButton(
-
-                            onPressed: isRewardedReady && rewardedAd != null
-
-                                ? () async {
-
-                                    await adsManager.showRewardedAd(
-
-                                        rewardedAd!, premiumManager);
-
-                                    await _saveRewardedTime();
-
-                                    rewardedAd = null;
-
-                                    isRewardedReady = false;
-
-                                    adsManager.loadRewardedAd(onLoaded: (ad) {
-
-                                      rewardedAd = ad;
-
-                                      isRewardedReady = true;
-
-                                    });
-
-                                    setState(() {}); // Refresh UI after reward
-
-                                  }
-
-                                : null,
-
-                            style: ElevatedButton.styleFrom(
-
-                              backgroundColor: Colors.amber,
-
-                              foregroundColor: Colors.black,
-
-                              padding: const EdgeInsets.symmetric(
-
-                                  vertical: 12, horizontal: 24),
-
-                            ),
-
-                            child: const Text("Watch Ad for Premium",
-
-                                style: TextStyle(fontSize: 16)),
-
-                          ),
-
-                        if (premiumManager.isPremium)
-
-                          Padding(
-
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-
-                            child: Text(
-
-                              "Premium Active: ${premiumManager.remaining.inMinutes} min left",
-
-                              style: const TextStyle(
-
-                                  fontSize: 14, fontWeight: FontWeight.w500),
-
-                            ),
-
-                          ),
+                            onPressed: () => setState(() => rightPanelOpen = false)),
 
                       ],
 
                     ),
 
-                  ),
+                    const Divider(),
 
-                ],
+                    Expanded(
+
+                      child: Column(
+
+                        mainAxisAlignment: MainAxisAlignment.center,
+
+                        children: [
+
+                          if (!premiumManager.isPremium)
+
+                            const Padding(
+
+                              padding: EdgeInsets.all(8.0),
+
+                              child: Text(
+
+                                  "Not a Premium User. Unlock features by watching ads.",
+
+                                  textAlign: TextAlign.center,
+
+                                  style: TextStyle(fontSize: 16)),
+
+                            ),
+
+                          if (!premiumManager.isPremium)
+
+                            ElevatedButton(
+
+                              onPressed: isRewardedReady && rewardedAd != null
+
+                                  ? () async {
+
+                                      await adsManager.showRewardedAd(
+
+                                          rewardedAd!, premiumManager);
+
+                                      await _saveRewardedTime();
+
+                                      rewardedAd = null;
+
+                                      isRewardedReady = false;
+
+                                      adsManager.loadRewardedAd(onLoaded: (ad) {
+
+                                        rewardedAd = ad;
+
+                                        isRewardedReady = true;
+
+                                      });
+
+                                      setState(() {});
+
+                                    }
+
+                                  : null,
+
+                              style: ElevatedButton.styleFrom(
+
+                                backgroundColor: Colors.amber, // golden button
+
+                                foregroundColor: Colors.black,
+
+                                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+
+                                shape: RoundedRectangleBorder(
+
+                                  borderRadius: BorderRadius.circular(12),
+
+                                ),
+
+                              ),
+
+                              child: const Text("Watch Ad to Unlock Premium", textAlign: TextAlign.center),
+
+                            ),
+
+                          if (premiumManager.isPremium)
+
+                            Padding(
+
+                              padding: const EdgeInsets.all(8.0),
+
+                              child: Text(
+
+                                "Premium Active!\nTime remaining: ${premiumManager.remaining.inMinutes.remainder(60).toString().padLeft(2,'0')}:${premiumManager.remaining.inSeconds.remainder(60).toString().padLeft(2,'0')}",
+
+                                textAlign: TextAlign.center,
+
+                                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+
+                              ),
+
+                            ),
+
+                          const SizedBox(height: 20),
+
+                          // Example premium features
+
+                          if (premiumManager.isPremium)
+
+                            Column(
+
+                              children: const [
+
+                                ListTile(
+
+                                  leading: Icon(Icons.flash_on),
+
+                                  title: Text("Faster Calculations"),
+
+                                ),
+
+                                ListTile(
+
+                                  leading: Icon(Icons.history_toggle_off),
+
+                                  title: Text("Unlimited History"),
+
+                                ),
+
+                                ListTile(
+
+                                  leading: Icon(Icons.lock_open),
+
+                                  title: Text("Unlock Advanced Functions"),
+
+                                ),
+
+                              ],
+
+                            ),
+
+                        ],
+
+                      ),
+
+                    ),
+
+                  ],
+
+                ),
 
               ),
 
